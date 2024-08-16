e584c31ffd40d54a3ec153e0918556aedf29a889
cog1t0 <riku4724@gmail.com>
2024-08-16 21:41:46 +0900

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add the hostname of the production environment
+  config.hosts << "rails-production-f915.up.railway.app"
 end
